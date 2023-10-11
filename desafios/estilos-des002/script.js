832bb3b54c0ff24fc9f45ccb4349d4d2cdcdcfeb
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-11 01:25:03 -0300

    .

--- desafios/estilos-des002/script.js
+++ desafios/estilos-des002/script.js
@@ -10,7 +10,45 @@ function verificar() {
         var sexo = document.getElementsByName('sexo')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var tamanho = document.getElementById('tamanho')
+        var img = document.getElementById('tamanho-img')
 
-        if (sexo)
+        if (sexo[0].checked) {
+
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+
+                img.setAttribute('src', 'estilos-des002/crianca-m.png')
+            } else if (idade < 21) {
+
+                img.setAttribute('src', 'estilos-des002/jovem-m.png')
+            } else if (idade < 50) {
+
+                img.setAttribute('src', 'estilos-des002/adulto-m.png')
+            } else {
+
+                img.setAttribute('src', 'estilos-des002/idoso-m.png')
+            }
+        } else if (sexo[1].checked) {
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                
+                img.setAttribute('src', 'estilos-des002/crianca-f.png')
+            } else if (idade < 21) {
+                
+                img.setAttribute('src', 'estilos-des002/jovem-f.png')
+            } else if (idade < 50) {
+                
+                img.setAttribute('src', 'estilos-des002/adulto-f.png')
+            } else {
+                
+                img.setAttribute('src', 'estilos-des002/idoso-f.png')
+            }
+        }
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+
+        res.style.textAlign = 'center'
     }
 }
