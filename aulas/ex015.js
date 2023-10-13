2935b82004b6e85310363698fa90625e79f5e236
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-12 22:55:34 -0300

    .

---
+++ aulas/ex015.js
@@ -0,0 +1,19 @@
+//var c = 1
+
+for (var c = 1; c <= 6; c++) {
+    console.log(`passo ${c}`)
+}
+
+
+/*do {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+
+*/
+
---
+++ aulas/ex016.js
@@ -0,0 +1,5 @@
+var c = 1
+
+while (c <= 2) {
+    console.log(c)
+}
