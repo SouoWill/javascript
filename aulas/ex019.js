5b1354f0da8e7c458ba3e9d06613e343aefa2ebf
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-15 20:26:31 -0300

    .

---
+++ aulas/ex019.js
@@ -0,0 +1,12 @@
+function soma(n1=0, n2=0) { //colocar =0 e um parametro pre definido caso o usuario só digite 1 numero
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
+
+/*
+let n1 = 2
+let n2 = 5
+
+console.log(n1 + n2)
+*/
