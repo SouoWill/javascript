6f0cdc826f36862f7180615c2841908f7e836867
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-14 23:36:43 -0300

    .

---
+++ desafios/arquivos-des003/script.js
@@ -0,0 +1,18 @@
+function tabuada() {
+    let num = document.getElementById('numero')
+    let tabuada = document.getElementById('tabuada')
+    
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        while (c <= 10) {
+            let = item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+}
