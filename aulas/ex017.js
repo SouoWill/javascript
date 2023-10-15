24a472a2f52f41486d70d992995f1d27dfd4d858
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-15 02:23:33 -0300

    .

--- aulas/ex017.js
+++ aulas/ex017.js
@@ -1,9 +1,14 @@
 let num = [5, 8, 9, 3, 2]
 num.push(7)  //ordem influencia
 console.log(num)
-console.log(`o terceiro vetor é ${num[2]}`) 
 console.log(`O vetor tem ${num.length} elementos.`)
-
+console.log(`O valor ${num[2]} esta na posição ${num.indexOf(9)}`)
+/*
 for(let pos = 0; pos < num.length; pos++) {
     console.log(`A posicão ${pos} tem o valor de ${num[pos]}.`)
+}
+*/
+
+for(let pos in num) {
+    console.log(`A posicão ${pos} tem o valor de ${num[pos]}.`)  //só funciona para arrays
 }
