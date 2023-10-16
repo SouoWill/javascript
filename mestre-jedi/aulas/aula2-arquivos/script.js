a4ae6d2750e77b9c45cdebd61eb4329ca1d0f2b3
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-16 19:40:31 -0300

    .

---
+++ mestre-jedi/aulas/aula2-arquivos/script.js
@@ -0,0 +1,17 @@
+//variáveis 
+
+let nome = 'juan'
+var sobreNome = 'william'
+
+// declaração
+function exibirNome() {
+    console.log(`${nome} ${sobreNome}`)
+}
+
+function exibirNomeCompleto() {
+    console.log(`Aluno 1, nome completo: ${nome} ${sobreNome}`)
+}
+
+//chamadas de função
+exibirNome() 
+exibirNomeCompleto()
