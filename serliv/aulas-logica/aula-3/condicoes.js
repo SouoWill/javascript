b81ef3182aa1ef7d19630a03b12d39371dad10e1
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-19 22:04:37 -0300

    .

---
+++ serliv/aulas-logica/aula-3/condicoes.js
@@ -0,0 +1,10 @@
+let idade = 21
+let paisPresente = false
+let comprouBilhete = false
+
+if(comprouBilhete) 
+{
+    console.log('Comprou o bilhete')
+} else {
+    console.log('Não comprou o bilhete')
+}
