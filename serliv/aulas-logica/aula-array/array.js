abe0f356e671a747c50fd756dfff0f65c013dff4
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-27 23:34:15 -0300

    .

---
+++ serliv/aulas-logica/aula-array/array.js
@@ -0,0 +1,13 @@
+const arr = new Array()
+const arr2 = new Array(true, "Daniel", 28, new Array(2, 4 ,10))
+arr[0] = "Juan"
+arr[1] = 40
+console.log(arr)
+console.log(arr2[3][2])
+
+// literal (mais usada)
+
+const arr3 = ['daniel', 40, [3, 6, 9], true]
+console.log(arr3)
+console.log(arr3[2])
+console.log(arr3[2][0])
