9c3e3a5b6d4eae094f72c759b71e91768d190bac
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-28 23:24:34 -0300

    .

---
+++ serliv/aulas-logica/aula-objetos/objects.js
@@ -0,0 +1,28 @@
+//array 
+/*const nomes = ['daniel', 'maria', 'juan']
+const idades = [40, 28, 17]
+
+console.log(nomes[0], idades[0])*/
+
+//linguagem formal
+
+/*const pessoa = new Object()
+pessoa.nome = 'daniel'
+pessoa.idade = 40
+
+console.log(pessoa)
+let prop = 'nome'
+console.log(pessoa[prop])
+console.log(pessoa.idade)*/
+
+//linguagem literal
+
+const pessoa = {
+    nome: 'daniel',
+    idade: 40
+}
+
+console.log(pessoa)
+let prop = 'nome'
+console.log(pessoa[prop])
+console.log(pessoa.idade)
---
+++ serliv/aulas-logica/aula-objetos/objescts-2.js
@@ -0,0 +1,6 @@
+const nomes = ['joao', 'juan', 'thayna', 'ygor']
+
+//length serve para ler quantos valores tem dentro de um array
+for(let i = 0; i < nomes.length; i++) {
+    console.log(`${i}: Nome é ${nomes[i]}`)
+}
