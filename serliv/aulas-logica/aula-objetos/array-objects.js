57e99a7f86b34c01a3c11b70092cf29729054993
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-29 23:42:14 -0300

    .

---
+++ serliv/aulas-logica/aula-objetos/array-objects.js
@@ -0,0 +1,18 @@
+const nomes = [ 'juan', 'carvalho', '' ]
+const pessoa = { nome: 'juan', idade: 17, email: '' }
+const pessoa1 = {}
+
+const pessoas = [ {
+    nome: 'juan',
+    idade: 17
+}, {
+    nome: 'thayna',
+    idade: 17
+}, {
+    nome: 'victor',
+    idade: 17
+}, {
+    nome: 'julia',
+    idade: 17
+} ]
+
