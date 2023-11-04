ec1cb47e1bb10a409d0fa0c68576c5d5f6d21c27
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-04 03:00:44 -0300

    .

--- serliv/desafios/desafio-calcular-imc/script.js
+++ serliv/desafios/desafio-calcular-imc/script.js
@@ -10,22 +10,22 @@ function calcularIMC() {
         return
     }
 
-    if(resIMC >= 16.0 && resIMC <= 16.9) 
+    if(resIMC <= 16.9) 
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está muito abaixo do peso!</p>`
-    } else if (resIMC >= 17.0 && resIMC <= 18.4) 
+    } else if (resIMC <= 18.4) 
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está abaixo do peso!</p>`
-    } else if (resIMC >= 18.5 && resIMC <= 24.9) 
+    } else if (resIMC <= 24.9) 
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está no peso ideal!</p>`
-    } else if (resIMC >= 25.0 && resIMC <= 29.9) 
+    } else if (resIMC <= 29.9) 
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está acima do peso!</p>`
-    } else if (resIMC >= 30.0 && resIMC <= 34.9)
+    } else if (resIMC <= 34.9)
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você com obesidade grau I!</p>`
-    } else if (resIMC >= 35.0 && resIMC <= 40.0)
+    } else if (resIMC <= 40.0)
     {
         imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você com obesidade grau II!</p>`
     } else if (resIMC > 40.0) {
