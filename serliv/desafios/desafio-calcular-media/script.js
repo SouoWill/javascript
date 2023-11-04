eef9724f4c0c7d854428c3313182d5e800f5612a
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-04 01:35:53 -0300

    .

--- serliv/desafios/desafio-calcular-media/script.js
+++ serliv/desafios/desafio-calcular-media/script.js
@@ -1,6 +1,21 @@
-let nota1 = Number(document.getElementById('nota1'))
-let nota2 = Number(document.getElementById('nota2'))
+function calcularMedia() {
+    const nota1 = parseFloat(document.getElementById('nota1').value)
+    const nota2 = parseFloat(document.getElementById('nota2').value)
+    let media = (nota1 + nota2) / 2
 
-function calcularMedia(n1, n2) {
-    
+    if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10) 
+    {
+        alert("Por favor, insira notas válidas entre 0 e 10.")
+        return 
+    }
+
+    if(media >= 8) 
+    {
+        document.getElementById('media').innerHTML = `<p>A media do aluno é: ${media.toFixed(1)} <br> ele esta <strong>acima</strong> da média!</p>`
+    } else if(media >= 6) {
+        document.getElementById('media').innerHTML = `<p>A media do aluno é: ${media.toFixed(1)} <br> ele esta na da média!</p>`
+    } else {
+        document.getElementById('media').innerHTML = `<p>A media do aluno é: ${media.toFixed(1)} <br> ele <strong>abaixo</strong> na da média!</p>`
+        return
+    }
 }
