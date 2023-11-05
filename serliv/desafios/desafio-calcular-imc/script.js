5274fc9842fd4db2c9039af46e20bf86f9429f47
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-05 13:33:00 -0300

    .

--- serliv/desafios/desafio-calcular-imc/script.js
+++ serliv/desafios/desafio-calcular-imc/script.js
@@ -1,34 +1,42 @@
-function calcularIMC() {
-    const peso = parseFloat(document.getElementById('peso').value)
-    const altura = parseFloat(document.getElementById('altura').value)
-    let imc = document.getElementById('imc')
-    let resIMC =  peso / (altura * altura)
+let calcularBtn = document.getElementById('calcular-btn');
 
-    if(peso > 150 || peso < 0 || altura > 3 || altura < 1) 
-    {
-        alert('Informe apenas dados válidos.')
-        return
+function calcularIMC(peso, altura) {
+    if (peso > 150 || peso < 0 || altura > 3 || altura < 1) {
+        alert('Informe apenas dados válidos.');
+        return -1; // Retorna um valor inválido para indicar erro
     }
+    return peso / (altura * altura);
+}
 
-    if(resIMC <= 16.9) 
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está muito abaixo do peso!</p>`
-    } else if (resIMC <= 18.4) 
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está abaixo do peso!</p>`
-    } else if (resIMC <= 24.9) 
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está no peso ideal!</p>`
-    } else if (resIMC <= 29.9) 
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você está acima do peso!</p>`
-    } else if (resIMC <= 34.9)
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você com obesidade grau I!</p>`
-    } else if (resIMC <= 40.0)
-    {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você com obesidade grau II!</p>`
-    } else if (resIMC > 40.0) {
-        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(1)}</strong> <br> Você com obesidade grau III!</p>`
+function classificarIMC(resIMC) {
+    if (resIMC < 0) {
+        return "Dados inválidos";
+    } else if (resIMC <= 16.9) {
+        return "Muito abaixo do peso";
+    } else if (resIMC <= 18.4) {
+        return "Abaixo do peso";
+    } else if (resIMC <= 24.9) {
+        return "Peso ideal";
+    } else if (resIMC <= 29.9) {
+        return "Acima do peso";
+    } else if (resIMC <= 34.9) {
+        return "Obesidade grau I";
+    } else if (resIMC <= 40.0) {
+        return "Obesidade grau II";
+    } else {
+        return "Obesidade grau III";
     }
 }
+
+calcularBtn.addEventListener('click', function() {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const imc = document.getElementById('imc');
+    const resIMC = calcularIMC(peso, altura);
+
+    if (resIMC !== -1) {
+        imc.innerHTML = `<p>Seu IMC é: <strong>${resIMC.toFixed(2)}</strong><br>Você está ${classificarIMC(resIMC)}.</p>`;
+    } else {
+        imc.innerHTML = `<p>Os dados inseridos são inválidos.</p>`;
+    }
+});
