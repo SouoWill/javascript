ee7ab48a7f2dead4cde59c184fce4394bb9835ef
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-09 01:28:38 -0300

    Create newsletter.js

---
+++ serliv/DOM/abilitandoedesabilitando-um-input/js/newsletter.js
@@ -0,0 +1,13 @@
+const txtEmail = document.getElementById("txtEmail")
+
+
+function editarEmail() {
+    txtEmail.disabled = false
+    txtEmail.focus()
+}
+
+function disabledEmail() {
+    txtEmail.disabled = true
+}
+
+
