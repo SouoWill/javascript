146711c81664a150bb69a46dc7a930263d564664
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-09 23:51:53 -0300

    .

--- serliv/desafios/desafio-checkbox-disable/checkbox_disabled.js
+++ serliv/desafios/desafio-checkbox-disable/checkbox_disabled.js
@@ -1,19 +1,11 @@
-(function () {
-    const $checkBox = document.getElementById('contrato').checked
-    const txtNome = document.getElementById('txtNome')
-    const txtEmail = document.getElementById('txtEmail')
-    const submit = document.getElementById('submit')
+function enableOrDisableButton() {
+    const checkBox = document.getElementById('contrato');
+    const submit = document.getElementById('submit');
 
-    function enableOrDisableButton() {
-        if ($checkBox === true) {
-            txtNome.disabled = true;
-            txtEmail.disabled = true;
-            submit.disabled = true;
-        } else {
-            txtNome.disabled = false;
-            txtEmail.disabled = false;
-            submit.disabled = false;
-        }
+    if (checkBox.checked === false) 
+    {
+        submit.disabled = true;
+    } else {
+        submit.disabled = false;
     }
-    enableOrDisableButton()
-}) ()   
+}
