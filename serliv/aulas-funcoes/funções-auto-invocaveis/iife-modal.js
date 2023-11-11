250d59c0e10f417eae82bc54b804d811008c8b8e
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-10 22:33:35 -0300

    Create iife-modal.js

---
+++ serliv/aulas-funcoes/funções-auto-invocaveis/iife-modal.js
@@ -0,0 +1,16 @@
+
+// function init() {
+//     let isValid = true
+//     console.log('init modal', isValid)
+// }
+// init()
+
+(function() {
+    let isValid = true
+    console.log('modal', isValid)
+
+    function init() {
+        console.log('init do modal')
+    }
+    init()
+})()
