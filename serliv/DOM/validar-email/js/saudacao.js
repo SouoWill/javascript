2750fa1d6f9cd2b6f76f15eed3c8cf48660cfc5a
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-10 22:35:05 -0300

    Delete saudacao.js

--- serliv/DOM/validar-email/js/saudacao.js
+++
@@ -1,6 +0,0 @@
-const msgFeedback = document.getElementById('newsletterFeedback')
-
-function validarEmail() {
-    const txtEmail = document.getElementById('txtEmail').value
-    msgFeedback.innerHTML = '<p>Validado com sucesso!'
-}
