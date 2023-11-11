6dd1b81fa6655d99ffb5db616abd0b06925c3253
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-10 22:44:20 -0300

    Update array01.js

--- serliv/arrays/array01.js
+++ serliv/arrays/array01.js
@@ -4,15 +4,19 @@ let sohNumeros = arr.some( function (el) {
     return typeof el === 'number' && el > 20
 })
 
-const arr1 = arr.filter( function (el, i, _arr) {
+let arr1 = arr.filter( function (el, i, _arr) {
     return typeof el === 'number'
 })
 
-const arr2 = []
+let arr2 = []
 
 arr.forEach( function (elemento, indice) {
     console.log(indice, ':', elemento)
 })
 
+let _arr2 = arr1.map( function (element, indic, _arr) {
+    return element * element
+})
+
 console.log(arr)
-console.log(arr1)
+console.log(_arr2)
