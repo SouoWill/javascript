c5ffe07fa838da136ae90cd900b7db5bde48de98
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-12 00:08:35 -0300

    .

--- serliv/arrays/arr02.js
+++ serliv/arrays/arr02.js
@@ -2,3 +2,13 @@ let arr = [4, 5, 10, 20, 20, 35, 4, 5]
 
 console.log( arr.indexOf(5) )
 console.log( arr.lastIndexOf(5) )
+
+console.log(arr.includes(5))
+
+console.log(arr.find( function(elemento) {
+    return elemento > 10
+}))
+
+console.log(arr.findIndex( function(elemento) {
+    return elemento > 10
+}))
