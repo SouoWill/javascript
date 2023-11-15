bb0c0b87bba1d9b106c077d0039f40a13c3ff6a1
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-11-14 23:39:57 -0300

    .

---
+++ serliv/arrays/array06.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3, 4]
+// arr.reverse()
+// console.log(arr)
+
+let j = 0
+
+let soma = arr.reduce( function(acumulador, atual, indice, _arr) {
+    console.log('i: ', indice)
+    console.log('j: ', j++)
+    console.log('acumulador: ', acumulador, '---', 'atual: ', atual)
+    return acumulador + atual
+}, 0)
+console.log(soma)
+
+const nomes = ['juan', 'thayna', 'victor', 'vitoria']
+let nomesPorOrdem = nomes.reduce( function(nomes, nomeAtual){
+    let primeiraLetra = nomeAtual[0]
+}, {} )
